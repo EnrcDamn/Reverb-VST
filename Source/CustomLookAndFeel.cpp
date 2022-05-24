4fa2d0e8242bd608b513648b1550941cf3726496
EnrcDamn <enrico.damiani10@gmail.com>
2022-05-25 00:45:41 +0200

    Create Look and feel class

--- Source/CustomLookAndFeel.cpp
+++ Source/CustomLookAndFeel.cpp
@@ -9,3 +9,37 @@
 */
 
 #include "CustomLookAndFeel.h"
+
+juce::Slider::SliderLayout CustomLookAndFeel::getSliderLayout(juce::Slider& slider)
+{
+
+}
+
+
+void CustomLookAndFeel::drawRotarySlider(juce::Graphics&, int x, int y, int width, int height,
+                                         float sliderPosProportional, float rotaryStartAngle,
+                                         float rotaryEndAngle, juce::Slider&)
+{
+
+}
+
+
+juce::Label* CustomLookAndFeel::createSliderTextBox(juce::Slider& slider)
+{
+
+}
+
+
+juce::Font CustomLookAndFeel::getTextButtonFont(juce::TextButton&, int buttonHeight)
+{
+
+}
+
+
+void CustomLookAndFeel::drawButtonBackground(juce::Graphics& g, juce::Button& button,
+                                             const juce::Colour& backgroundColour,
+                                             bool shouldDrawButtonAsHighlighted,
+                                             bool shouldDrawButtonAsDown)
+{
+
+}
